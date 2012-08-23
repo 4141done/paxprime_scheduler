fcf281865dc5ac5af1b2ffc5982ff42157eaa780
Javier Evans <Javier.Evans@citygridmedia.com>
2012-08-22 23:59:14 -0700

    fixing some string concat issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,17 @@ class User < ActiveRecord::Base
   has_one :schedule
   has_many :schedule_entries,  :through => :schedule
   has_many :shared_schedules
-  
+
   def self.generate_identifier
     name_material = ["http://seattle.craigslist.org/zip/", "http://seattle.craigslist.org/jjj/", "http://seattle.craigslist.org/ccc/"]
     agent = Mechanize.new
     page = agent.get name_material[rand(2)]
     link1 = page.links[rand(page.links.length)].text.split(" ")
     link2 = page.links[rand(page.links.length)].text.split(" ")
-    possible_identifier = link1[rand(link1.length)].to_s + "_" + link2[rand(link2.length)]
+    possible_identifier = "#{link1[rand(link1.length)]}_#{link2[rand(link2.length)]}"
 
     if possible_identifier == nil
-      possible_identifier = (link1[rand(link1.length)].to_s + "_" + link2[rand(link2.length)])
+      possible_identifier = "#{(link1[rand(link1.length)]}_#{link2[rand(link2.length)])}"
     end
 
     return possible_identifier.scan(/\w+/).join("_").downcase
